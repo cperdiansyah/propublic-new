0705cb5435a33b44b7be07d04e433535e50ee0f1
cperdiansyah <chandraperdiansyah@gmail.com>
2025-07-04 05:19:59 +0700

    feat : prepare resources for carousel card

---
+++ src/config/exampleData.ts
@@ -0,0 +1,30 @@
+export const GAMELIST: CarouselGameItem[] = [
+  {
+    id: 1,
+    name: 'Valorant',
+    slug: 'valorant',
+    path: '/games/valorant',
+    imageSrc: '/images/game-covers/valorant.jpg',
+  },
+  {
+    id: 2,
+    name: 'Apex Legends',
+    slug: 'apex-legends',
+    path: '/games/apex-legends',
+    imageSrc: '/images/game-covers/apex-legends.png',
+  },
+  {
+    id: 3,
+    name: 'Mobile Legends',
+    slug: 'mobile-legends',
+    path: '/games/mobile-legends',
+    imageSrc: '/images/game-covers/mobile-legends.jpg',
+  },
+  {
+    id: 4,
+    name: 'Dota 2',
+    slug: 'dota-2',
+    path: '/games/dota-2',
+    imageSrc: '/images/game-covers/dota-2.jpg',
+  },
+] as const
