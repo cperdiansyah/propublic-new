739fbba0ee81c42173901392df80817b1ac6f5f1
cperdiansyah <chandraperdiansyah@gmail.com>
2025-07-15 07:17:09 +0700

    chore : update next config files

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,9 @@ const nextConfig: NextConfig = {
       new URL('https://propublic-academy.s3.ap-southeast-1.amazonaws.com/**'),
       new URL('https://**.amazonaws.com/**'),
       new URL('https://avatar.iran.liara.run/**'),
+      // new URL('https://images.unsplash.com/**/**'),
     ],
+    domains: ['images.unsplash.com', 'avatar.iran.liara.run'],
   },
 }
 
