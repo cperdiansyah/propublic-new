25c0093c63a069c5832ba2f6c906c5dbd17edb2d
Ruyatama <andikakandrianto.w@gmail.com>
2025-07-16 11:46:08 +0700

    feat: adjust mobile navigation

--- src/components/layout/navigation/mobile-navigation.tsx
+++ src/components/layout/navigation/mobile-navigation.tsx
@@ -16,11 +16,11 @@ interface NavItem {
 const MobileBottomNav = () => {
   const pathname = usePathname()
   return (
-    <nav className="fixed bottom-0 left-0 right-0 z-50 bg-gray  border-t border-gray-200/ md:hidden">
+    <nav className="fixed bottom-0 left-0 right-0 z-50 bg-gray border-t border-gray-200/ md:hidden">
       <div
         className={cn(
-          'flex items-center justify-around px-2 py-4 ',
-          'bg-dark-primary  ',
+          'flex items-center justify-around px-2 py-2',
+          'bg-dark-primary',
         )}
       >
         {navigation.map((item) => {
@@ -46,20 +46,20 @@ function NavigationItem({
     <Link
       key={href}
       href={href}
-      className={`relative flex flex-col items-center justify-center p-4 w-10 h-10 !rounded-lg transition-all duration-200 ${
+      className={`relative flex flex-col items-center justify-center p-4 w-12 h-12 !rounded-md transition-all duration-200 ${
         isActive
-          ? 'bg-blue-100 text-custom-primary '
+          ? 'bg-blue-100 text-custom-primary'
           : 'text-white hover:text-gray-700 hover:bg-gray-50'
       }`}
     >
       {/* Square background for active state */}
       {isActive && (
-        <div className="absolute inset-0 bg-[#7B001F] rounded-lg transition-all duration-200" />
+        <div className="absolute inset-0 rounded-md transition-all duration-200" />
       )}
 
       {/* Icon container with badge */}
-      <div className="relative z-10 mb-1">
-        <Icon className={`h-5 w-5 ${isActive && 'text-custom-primary'}`} />
+      <div className="relative z-10">
+        <Icon className={`h-6 w-6`} />
 
         {notificationCount && (
           <div className="indicator">
